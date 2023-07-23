6bb814d97a705c2f449d04083a8b09b1914d1be3
Mahroo Hajimehdi <106196835+mahroo-hm@users.noreply.github.com>
2023-07-23 17:28:08 +0330

    Create palindrom.asm

---
+++ palindrom.asm
@@ -0,0 +1,330 @@
+%ifndef SYS_EQUAL
+%define SYS_EQUAL
+
+    sys_read     equ     0
+    sys_write    equ     1
+    sys_open     equ     2
+    sys_close    equ     3
+    
+    sys_lseek    equ     8
+    sys_create   equ     85
+    sys_unlink   equ     87
+      
+
+    sys_mkdir       equ 83
+    sys_makenewdir  equ 0q777
+
+
+    sys_mmap     equ     9
+    sys_mumap    equ     11
+    sys_brk      equ     12
+    
+     
+    sys_exit     equ     60
+    
+    stdin        equ     0
+    stdout       equ     1
+    stderr       equ     3
+
+ 
+	PROT_NONE	  equ   0x0
+    PROT_READ     equ   0x1
+    PROT_WRITE    equ   0x2
+    MAP_PRIVATE   equ   0x2
+    MAP_ANONYMOUS equ   0x20
+    
+    ;access mode
+    O_DIRECTORY equ     0q0200000
+    O_RDONLY    equ     0q000000
+    O_WRONLY    equ     0q000001
+    O_RDWR      equ     0q000002
+    O_CREAT     equ     0q000100
+    O_APPEND    equ     0q002000
+
+
+    BEG_FILE_POS    equ     0
+    CURR_POS        equ     1
+    END_FILE_POS    equ     2
+    
+; create permission mode
+    sys_IRUSR     equ     0q400      ; user read permission
+    sys_IWUSR     equ     0q200      ; user write permission
+
+    NL            equ   0xA
+    Space         equ   0x20
+
+%endif
+
+%ifndef NOWZARI_IN_OUT
+%define NOWZARI_IN_OUT
+
+;----------------------------------------------------
+newLine:
+   push   rax
+   mov    rax, NL
+   call   putc
+   pop    rax
+   ret
+;---------------------------------------------------------
+putc:	
+
+   push   rcx
+   push   rdx
+   push   rsi
+   push   rdi 
+   push   r11 
+
+   push   ax
+   mov    rsi, rsp    ; points to our char
+   mov    rdx, 1      ; how many characters to print
+   mov    rax, sys_write
+   mov    rdi, stdout 
+   syscall
+   pop    ax
+
+   pop    r11
+   pop    rdi
+   pop    rsi
+   pop    rdx
+   pop    rcx
+   ret
+;---------------------------------------------------------
+writeNum:
+   push   rax
+   push   rbx
+   push   rcx
+   push   rdx
+
+   sub    rdx, rdx
+   mov    rbx, 10 
+   sub    rcx, rcx
+   cmp    rax, 0
+   jge    wAgain
+   push   rax 
+   mov    al, '-'
+   call   putc
+   pop    rax
+   neg    rax  
+
+wAgain:
+   cmp    rax, 9	
+   jle    cEnd
+   div    rbx
+   push   rdx
+   inc    rcx
+   sub    rdx, rdx
+   jmp    wAgain
+
+cEnd:
+   add    al, 0x30
+   call   putc
+   dec    rcx
+   jl     wEnd
+   pop    rax
+   jmp    cEnd
+wEnd:
+   pop    rdx
+   pop    rcx
+   pop    rbx
+   pop    rax
+   ret
+
+;---------------------------------------------------------
+getc:
+   push   rcx
+   push   rdx
+   push   rsi
+   push   rdi 
+   push   r11 
+
+ 
+   sub    rsp, 1
+   mov    rsi, rsp
+   mov    rdx, 1
+   mov    rax, sys_read
+   mov    rdi, stdin
+   syscall
+   mov    al, [rsi]
+   add    rsp, 1
+
+   pop    r11
+   pop    rdi
+   pop    rsi
+   pop    rdx
+   pop    rcx
+
+   ret
+;---------------------------------------------------------
+
+readNum:
+   push   rcx
+   push   rbx
+   push   rdx
+
+   mov    bl,0
+   mov    rdx, 0
+rAgain:
+   xor    rax, rax
+   call   getc
+   cmp    al, '-'
+   jne    sAgain
+   mov    bl,1  
+   jmp    rAgain
+sAgain:
+   cmp    al, NL
+   je     rEnd
+   cmp    al, ' ' ;Space
+   je     rEnd
+   sub    rax, 0x30
+   imul   rdx, 10
+   add    rdx,  rax
+   xor    rax, rax
+   call   getc
+   jmp    sAgain
+rEnd:
+   mov    rax, rdx 
+   cmp    bl, 0
+   je     sEnd
+   neg    rax 
+sEnd:  
+   pop    rdx
+   pop    rbx
+   pop    rcx
+   ret
+
+;-------------------------------------------
+printString:
+   push    rax
+   push    rcx
+   push    rsi
+   push    rdx
+   push    rdi
+
+   mov     rdi, rsi
+   call    GetStrlen
+   mov     rax, sys_write  
+   mov     rdi, stdout
+   syscall 
+   
+   pop     rdi
+   pop     rdx
+   pop     rsi
+   pop     rcx
+   pop     rax
+   ret
+;-------------------------------------------
+; rdi : zero terminated string start 
+GetStrlen:
+   push    rbx
+   push    rcx
+   push    rax  
+
+   xor     rcx, rcx
+   not     rcx
+   xor     rax, rax
+   cld
+         repne   scasb
+   not     rcx
+   lea     rdx, [rcx -1]  ; length in rdx
+
+   pop     rax
+   pop     rcx
+   pop     rbx
+   ret
+;-------------------------------------------
+
+%endif
+
+
+section .data
+    pal: db 'abccba', 0
+    no: db 'NO', 10, 0
+    yes: db 'YES', 10, 0
+    length dq 0
+
+section .bss
+    p: resb 1000
+    ps: resb 1000
+
+section .text
+    global _start
+
+_start:
+
+mov rdx, 1000 ; number of bytes to read
+mov rcx, ps ; reserved space to store our input (known as a buffer)
+mov rbx, 0 ; read from the STDIN file
+mov rax, 3 ; invoke SYS_READ (kernel opcode 3)
+int 80h
+
+xor rcx, rcx ;loop counter
+xor r9, r9
+xor rax, rax
+
+mov rcx, ps
+mov rdx, p
+mov r9, p
+mov al, [rcx]
+inc qword[length]
+loop1:   
+    cmp al, NL 
+    je end
+    cmp al, ' '
+    je cont
+    inc qword[length]
+    mov [rdx], al
+    inc rdx
+    xor rax, rax
+cont:
+    inc rcx
+    mov al, [rcx]
+    jmp loop1
+    
+end:
+
+xor r9, r9 ; stores input len
+xor rdx, rdx
+xor rax, rax
+
+mov r9, qword[length]
+
+dec r9
+dec r9
+
+xor r8, r8
+xor rcx, rcx
+xor rdx, rdx
+
+xor r10, p
+mov r11, r9
+mov r12, p 
+add r12, r9
+
+xor rax, rax
+
+compare:
+  mov al, [r12]
+  cmp al, [r10]
+  jnz notPal
+  
+  
+  inc r10 
+  dec r12 
+  cmp r10, r12 
+  jae isPal
+  jmp compare
+
+notPal:
+    mov rsi, no
+    call printString
+    call Exit
+
+isPal:
+    mov rsi, yes
+    call printString
+    call Exit
+
+Exit:
+    mov rax, sys_exit
+    mov rdi, rdi
+    syscall
